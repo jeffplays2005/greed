03378cb99c81fc5ebae259c4b3268d892d39ad2e
Jeffery <61447509+jeffplays2005@users.noreply.github.com>
2025-08-30 22:13:42 +1200

    feat: add environment variables and types

---
+++ environment.d.ts
@@ -0,0 +1,27 @@
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      /**
+       * The Discord bot token.
+       */
+      DISCORD_TOKEN: string;
+      /**
+       * The Discord bot application ID.
+       */
+      DISCORD_APPLICATION_ID: string;
+      /**
+       * The Discord bot public key.
+       */
+      DISCORD_PUBLIC_KEY: string;
+
+      /**
+       * The Postgres database URL.
+       */
+      DB_URL: string;
+      /**
+       * The environment in which the application is running.
+       */
+      NODE_ENV: "development" | "staging" | "production";
+    }
+  }
+}
