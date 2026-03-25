2ff3d520dbe588fc11f8614a2f298ca9585f2cf6
Jeffery <61447509+jeffplays2005@users.noreply.github.com>
2026-03-25 17:56:42 +1300

    style: run biome

--- apps/bot/src/config/config.ts
+++ apps/bot/src/config/config.ts
@@ -2,11 +2,11 @@ const Config = {
   prefix: ",",
   defaultHexColor: "#2f3136",
   users: {
-    dev: ["543185949803151370"]
+    dev: ["543185949803151370"],
   },
   servers: {
-    support: ["721726753147453583"]
-  }
+    support: ["721726753147453583"],
+  },
 }
 
 export { Config }
