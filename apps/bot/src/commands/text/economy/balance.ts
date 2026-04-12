b84fa112a111d19783db9eb328c298234f861581
Jeffery <61447509+jeffplays2005@users.noreply.github.com>
2026-04-12 19:23:09 +1200

    refactor(balance-cmd): remove requirement to join team

--- apps/bot/src/commands/text/economy/balance.ts
+++ apps/bot/src/commands/text/economy/balance.ts
@@ -1,23 +1,14 @@
 import type { BaseCommandConfig, BaseCommandProps } from "@/types/command"
 import { createSimpleEmbed } from "@/utils/embeds"
 
-export const run = async ({ message, color, db, prefix, bot }: BaseCommandProps) => {
+export const run = async ({ message, color, db, bot }: BaseCommandProps) => {
   const userData = await db.users.getUserById(message.author.id)
-  if (!userData || !userData.balance)
-    return message.reply({
-      embeds: [
-        createSimpleEmbed(
-          `${message.author} hasn't been registered to a team yet. \`${prefix}register\` to register!`,
-          color,
-        ),
-      ],
-      allowedMentions: {},
-    })
+  const bal = userData?.userId ? userData.balance : 100
 
   return message.reply({
     embeds: [
       createSimpleEmbed(
-        `${message.author}'s balance is ${userData.balance} ${bot.config.emojis.balance}`,
+        `${message.author}'s balance is ${bal} ${bot.config.emojis.balance}`,
         color,
       ),
     ],
