537940c62a8d084f7ff40f6c34874d3fdaf9dfcd
Jeffery <61447509+jeffplays2005@users.noreply.github.com>
2026-04-18 00:44:18 +1200

    refactor(color): correct colors to use hex and number for new containers

--- apps/bot/src/config/config.ts
+++ apps/bot/src/config/config.ts
@@ -1,7 +1,8 @@
 const Config = {
   prefix: ",",
   devPrefix: ",,",
-  defaultHexColor: "#2f3136",
+  defaultHexColor: 0x2f3136,
+  defaultColor: "#2f3136",
   users: {
     dev: ["543185949803151370"],
     owners: ["543185949803151370", "761094139525201920", "662585716491943946"],
