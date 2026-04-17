edddf5554243f486c9f095e2604376bd219e911d
Jeffery <61447509+jeffplays2005@users.noreply.github.com>
2026-04-18 00:19:55 +1200

    feat(db): add server collection

--- apps/bot/src/database/collections/index.ts
+++ apps/bot/src/database/collections/index.ts
@@ -1,8 +1,10 @@
 import { payload } from "../adapters/Payload"
 import { ConfessionCollection } from "./ConfessionCollection"
+import { ServerCollection } from "./ServerCollection"
 import { UserCollection } from "./UserCollection"
 
 export const db = {
   users: new UserCollection(payload),
   confessions: new ConfessionCollection(payload),
+  servers: new ServerCollection(payload),
 }
