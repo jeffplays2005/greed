87360b39b5426fb7637fba2b13735462b4f8d26c
Jeffery <61447509+jeffplays2005@users.noreply.github.com>
2026-04-18 17:34:01 +1200

    refactor(types): combine command config and run type into single file

--- apps/bot/src/types/command/Command.ts
+++ apps/bot/src/types/command/Command.ts
@@ -2,7 +2,11 @@ import type { ColorResolvable, Message } from "discord.js"
 import type { Payload } from "payload"
 import type { db } from "@/database/collections"
 import type { Client } from "../Client"
-import type { BaseCommandConfig } from "./Config"
+import type {
+  DmsOnlyPermissionSet,
+  GuildOnlyPermissionSet,
+  NoLocationRestrictionPermissionSet,
+} from "./Permissions"
 
 /**
  * The return type of parsed command
@@ -18,6 +22,54 @@ export type CommandModule = {
    * can safely assume they're within a guild context and not worry about whether they're in a DM.
    */
   run<IsGuild extends boolean = boolean>(props: BaseCommandProps<IsGuild>): Promise<void> | void
+}
+
+/**
+ * The base command config type used to unionise typings
+ */
+export type BaseCommandConfig = {
+  /**
+   * The name of the command
+   */
+  name: string
+  /**
+   * The description of the command (used for help command)
+   */
+  description: string
+  /**
+   * A list of usages
+   * @remarks Do not include the prefix within the usage, this is appended
+   */
+  usage: string[]
+  /**
+   * A list of aliases
+   */
+  aliases?: string[]
+  /**
+   * The cooldown in seconds for this command, use 0 for no cooldown
+   * @example 1
+   */
+  cooldown?: number
+  /**
+   * Various permission config options
+   */
+  permissionSet?: GuildOnlyPermissionSet | DmsOnlyPermissionSet | NoLocationRestrictionPermissionSet
+  /**
+   * If the command is disabled
+   * @default false
+   */
+  disabled?: boolean
+  /**
+   * If the command is dev only
+   * @remarks Takes prescedence over other permissions
+   * @default false
+   */
+  dev?: boolean
+  /**
+   * If the command should be hidden away from the help command
+   * @default false
+   */
+  hide?: boolean
 }
 
 /**
